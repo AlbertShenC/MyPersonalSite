b9b30a2eb1b12775f41732ff20101807c6d2d3d5
AlbertShenC <30514009+AlbertShenC@users.noreply.github.com>
2019-12-24 18:55:20 +0800

    blog update function

--- code/MyPersonalSite/blog/urls.py
+++ code/MyPersonalSite/blog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('detail/<int:blog_id>/', views.blog_detail, name='blog_detail'),
     path('create/', views.blog_create, name='blog_create'),
     path('delete/<int:blog_id>/', views.blog_delete, name='blog_delete'),
+    path('update/<int:blog_id>/', views.blog_update, name='blog_update'),
 ]
--- code/MyPersonalSite/blog/views.py
+++ code/MyPersonalSite/blog/views.py
@@ -27,7 +27,7 @@ def blog_detail(request, blog_id):
 
 def blog_create(request):
     if request.method == 'POST':
-        blog_post_form = BlogPostForm(data = request.POST)
+        blog_post_form = BlogPostForm(data=request.POST)
         if blog_post_form.is_valid():
             new_blog = blog_post_form.save(commit = False)
             new_blog.author = User.objects.get(id = 1)
@@ -39,6 +39,25 @@ def blog_create(request):
         blog_post_form = BlogPostForm()
         context = {'blog_post_form': blog_post_form}
         return render(request, 'blog/create.html', context)
+
+
+def blog_update(request, blog_id):
+    blog = BlogPost.objects.get(id=blog_id)
+    if request.method == 'POST':
+        blog_post_form = BlogPostForm(data=request.POST)
+        if blog_post_form.is_valid():
+            blog.title = request.POST['title']
+            blog.body = request.POST['body']
+            blog.save()
+            return redirect('blog:blog_detail', blog_id)
+        else:
+            return HttpResponse('表单内容有误，请重新填写。')
+    else:
+        blog_post_form = BlogPostForm()
+        context = {
+            'blog': blog,
+            'blog_post_form': blog_post_form}
+        return render(request, 'blog/update.html', context)
 
 
 def blog_delete(request, blog_id):
